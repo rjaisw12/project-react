dded7b8e578aca33eb3c26d38c41e7f5f7ccd672
rjaisw12 <rjaiswal@hotmail.fr>
2023-06-01 12:31:52 +0200

    update front

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -50,7 +50,7 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="#"><img src={navIcon1} alt="" /></a>
+                <a href="https://www.linkedin.com/in/raphael-jaiswal/"><img src={navIcon1} alt="Linkedin Icon" /></a>
                 <a href="#"><img src={navIcon2} alt="" /></a>
                 <a href="#"><img src={navIcon3} alt="" /></a>
               </div>
