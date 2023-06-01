dc7ce3c869e5990f80f9bb96e118b76ae8f7e4cc
rjaisw12 <rjaiswal@hotmail.fr>
2023-06-01 13:16:42 +0200

    add url to projects

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,17 @@
 import { Col } from "react-bootstrap";
 
-export const ProjectCard = ({ title, description, imgUrl }) => {
+export const ProjectCard = ({ title, description, imgUrl, projectUrl }) => {
   return (
     <Col size={12} sm={6} md={4}>
+      <a href={projectUrl} target="_blank" rel="noopener noreferrer">
       <div className="proj-imgbx">
-        <img src={imgUrl} />
+        <img src={imgUrl} alt={title} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
       </div>
+      </a>
     </Col>
   )
 }
