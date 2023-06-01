512f744aeeb36b3af4a53354024e6fa9ad0724ff
rjaisw12 <rjaiswal@hotmail.fr>
2023-06-01 14:05:02 +0200

    change navicons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,8 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { MailchimpForm } from "./MailchimpForm";
 import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIconLinkedin from '../assets/img/nav-icon-linkedin.svg';
+import navIconGithub from '../assets/img/nav-icon-github.svg';
 
 export const Footer = () => {
   return (
@@ -15,9 +14,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a href="https://www.linkedin.com/in/raphael-jaiswal/"><img src={navIconLinkedin} alt="Linkedin Icon" /></a>
+                <a href="https://github.com/rjaisw12"><img src={navIconGithub} alt="Github Icon" /></a>
             </div>
             <p>Copyright 2022. All Rights Reserved</p>
           </Col>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import logo from '../assets/img/logo.svg';
-import navIcon1 from '../assets/img/nav-icon1.svg';
-import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
+import navIconLinkedin from '../assets/img/nav-icon-linkedin.svg';
+import navIconGithub from '../assets/img/nav-icon-github.svg';
 import { HashLink } from 'react-router-hash-link';
 import {
   BrowserRouter as Router
@@ -50,9 +49,8 @@ export const NavBar = () => {
             </Nav>
             <span className="navbar-text">
               <div className="social-icon">
-                <a href="https://www.linkedin.com/in/raphael-jaiswal/"><img src={navIcon1} alt="Linkedin Icon" /></a>
-                <a href="#"><img src={navIcon2} alt="" /></a>
-                <a href="#"><img src={navIcon3} alt="" /></a>
+                <a href="https://www.linkedin.com/in/raphael-jaiswal/"><img src={navIconLinkedin} alt="Linkedin Icon" /></a>
+                <a href="https://github.com/rjaisw12"><img src={navIconGithub} alt="Github Icon" /></a>
               </div>
               <HashLink to='#connect'>
                 <button className="vvd"><span>Let’s Connect</span></button>
