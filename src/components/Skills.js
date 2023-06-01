012ea5f937c1a748a2b70187fd64575c49ee154c
rjaisw12 <rjaiswal@hotmail.fr>
2023-06-01 12:56:56 +0200

    update skills and footer

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,19 +39,19 @@ export const Skills = () => {
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Python</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>Computer Vision</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>NLP</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Recommender Systems</h5>
                             </div>
                         </Carousel>
                     </div>
