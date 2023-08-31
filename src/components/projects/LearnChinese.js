e72f94b6ee7fad23c424c9a8f855110a32bfbc70
Raphaël Jaiswal <rjaiswal@hotmail.fr>
2023-08-31 21:01:56 +0200

    update LearnChinese.js

--- src/components/projects/LearnChinese.js
+++ src/components/projects/LearnChinese.js
@@ -5,29 +5,50 @@ const LearnChinese = () => {
   const [translatedSentence, setTranslatedSentence] = useState('');
   const [characterOptions, setCharacterOptions] = useState([]);
   const [selectedCharacters, setSelectedCharacters] = useState([]);
+  const [wordsToGuess, setWordsToGuess] = useState([]);
+  const [feedbackMessage, setFeedbackMessage] = useState('');
 
   const fetchData = async () => {
     try {
       const response = await fetch('https://apichinese.raphaeljaiswal.com/get-chinese-quiz');
-  
+
       if (!response.ok) {
         throw new Error('Erreur réseau lors de la récupération des données.');
       }
-  
+
       const data = await response.json();
       setSentenceWithMissingWord(data.sentenceWithMissingWord);
       setTranslatedSentence(data.translatedSentence);
       setCharacterOptions(data.characterOptions);
+      setWordsToGuess(data.words_to_guess);
     } catch (error) {
       console.error("Il y a eu un problème avec l'opération fetch: ", error.message);
     }
   };
-  
+
 
   const handleCharacterClick = (char) => {
     if (selectedCharacters.length < 2 && !selectedCharacters.includes(char)) {
       setSelectedCharacters([...selectedCharacters, char]);
     }
+  };
+
+  const checkAnswer = () => {
+    if (JSON.stringify(selectedCharacters) === JSON.stringify(wordsToGuess)) {
+      setFeedbackMessage('Well done!');
+    } else {
+      setFeedbackMessage('Wrong answer');
+    }
+  };
+
+  const resetAnswer = () => {
+    setSelectedCharacters([]);
+    setFeedbackMessage('');
+  };
+
+  const loadNextSentence = () => {
+    fetchData();
+    resetAnswer();
   };
 
   useEffect(() => {
@@ -51,6 +72,10 @@ const LearnChinese = () => {
           <span key={index}>{char} </span>
         ))}
       </div>
+      <button onClick={checkAnswer}>Check Answer</button>
+      <button onClick={resetAnswer}>Try New Answer</button>
+      <button onClick={loadNextSentence}>Next Sentence</button>
+      <p>{feedbackMessage}</p>
     </div>
   );
 };
